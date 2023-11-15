8855babb692dbd88c80fbf5aa198713beab0ba08
mohamed-zakariya <113525757+mohamed-zakariya@users.noreply.github.com>
2023-11-15 04:46:25 +0200

    assembly_part2

--- assembly.py
+++ assembly.py
@@ -5,17 +5,27 @@ import symbolTable as st
 class assembly:    
     def __init__(self):
         self.assembly_path = "assembly.txt"
+        self.assemblyTable = []
         self.locationCounter = []
         self.start_t_record={}
         self.wb = []
         self.reswb = []
 
+    def get_resw_element(self):
+    
+        if len(self.reswb) > 1:
+            if(int(self.reswb[1][0], 16) - int(self.reswb[0][0], 16) < int(self.reswb[0][1], 16) - int(self.reswb[0][0], 16)):
+                diff = int(self.reswb[1][0], 16) - int(self.reswb[0][0], 16)
 
-    def get_diff(start_t_record):
-        for i in range (len(start_t_record)):
-            if(hex(int(start_t_record[i+1],16)-int(start_t_record[i],16)) not in start_t_record[i]):
-                return start_t_record[i],start_t_record[i+1]
-
+            else:
+                diff = int(self.reswb[0][1], 16) - int(self.reswb[0][0], 16)
+                
+            self.reswb.pop(0)
+            return diff
+        diff = int(self.reswb[0][1], 16) - int(self.reswb[0][0], 16)
+        self.reswb.pop(0)
+        return diff
+        
 
     def get_resw(self, s):
         for key in s.inst_ref.keys():
@@ -24,19 +34,18 @@ class assembly:
                 lower_limit = int(s.recordLimits[i][1], 16)
                 upper_limit = int(s.recordLimits[i+1][0], 16)
                 value = int(key, 16)
-                # print(key)
-                # print( not ( lower_limit <= value < upper_limit) )
-                if   (lower_limit <= value < upper_limit)  and key in s.inst_ref and key not in self.reswb:
-                    print(s.recordLimits[i][1], " -> ",key, " -> ", s.recordLimits[i+1][0], "key is ", key)
+            
+                if(lower_limit <= value < upper_limit)  and key in s.inst_ref and key not in self.reswb:
                     self.reswb.append([key, s.recordLimits[i+1][0]])
                     break;
                 i += 1
 
-        sort = sorted(self.reswb, key=lambda x: int(x[0], 16)) 
-        unique_data = list(set(tuple(row) for row in sort))   
-        self.reswb = [list(row) for row in unique_data]
-        # sort = sorted(int(x, 16) for x in self.reswb)
-        # self.reswb = [hex(x)[2:].upper() for x in sorted_data]
+        unique_data = list(set(tuple(row) for row in self.reswb))   
+        unique_data = [list(row) for row in unique_data]
+        self.reswb = sorted(unique_data, key=lambda x: int(x[0], 16)) 
+
+      
+        
 
             
     def save_objectCodes(self, s, f):
@@ -48,9 +57,8 @@ class assembly:
             objcodes = f.lines[i][9:-1]
             j = 0
             while j < len(objcodes):
-            # print(opCode, ' ->', i, '->', )
                 opCode = objcodes[j:j+2]
-                if s.check_Format1():
+                if opCode in s.objcode_1:
                     s.objectCodes.append(objcodes[j:j+2])
                     j = j+2
                 else:
@@ -60,11 +68,19 @@ class assembly:
                 counter += 1
  
             i += 1
-        # print(objectCodes)
         return   counter  
     
+    def check_res(self, target_value):
+        i = 0
+        while i < len(self.reswb):
+            if target_value == self.reswb[i][0]:
+                return False
+            i += 1
+        return True
+
+
     def get_assembly_code(self, s,f):
-        pointerLC = hex(int(s.hRecord["start"], 16) +3)
+        pointerLC = hex(int(s.hRecord["start"], 16))
         pointerLC = pointerLC[2:]
         end = s.hRecord["end"]
         self.locationCounter.append(pointerLC)
@@ -73,34 +89,27 @@ class assembly:
         self.get_resw(s)
 
         i = 0
-        # pointerLC != end
         while i < counter:
-            # if self.get_diff(self.start_t_record):
-            #     # self.get_res(locationCounter)
-            #     print(1)
-            # print(pointerLC)   
-            if pointerLC not in s.symbolTable :
-                pointerLC = hex(int(pointerLC, 16) +3)
-                pointerLC = pointerLC[2:].upper()
-                # print("1")
+    
+            if pointerLC not in s.symbolTable and s.objectCodes[i] in s.objcode_1:
+                pointerLC = hex(int(pointerLC, 16) +1)
+            
+            
+            elif pointerLC not in s.symbolTable  or pointerLC in s.symbolTable and s.inst_ref[pointerLC] in d.opCode_j:
+                pointerLC = hex(int(pointerLC, 16) + 3)
 
 
-            elif pointerLC in s.symbolTable and s.inst_ref[pointerLC] in d.opCode_j:
-                pointerLC = hex(int(pointerLC, 16) +3)
-                pointerLC = pointerLC[2:].upper()
-                # print("2")
-
-            elif pointerLC in s.symbolTable and s.inst_ref[pointerLC] in d.opcode_3:
+            elif pointerLC in s.symbolTable and s.inst_ref[pointerLC] in d.opcode_3 and  self.check_res(pointerLC):
                 self.wb.append(pointerLC)
                 pointerLC = hex(int(pointerLC, 16) + int(len(s.objectCodes[i]) / 2))
-                pointerLC = pointerLC[2:].upper()
-                # print("3")
-            
-            
+
+            else:
+                pointerLC = hex(int(pointerLC, 16) + int(self.get_resw_element()))
+                i -=1
+
+            pointerLC = pointerLC[2:].upper()
             self.locationCounter.append(pointerLC)
             i += 1 
-        # print(self.locationCounter)
-
 
 
         
--- main.py
+++ main.py
@@ -10,22 +10,10 @@ def mainfun(f,s, ass):
     st.call_objectcode(f,s)
     s.get_trecord_symbolTable()
     s.create_symbol_table()
-    # print(f.lines)
-    # s.get_H_record(f.lines)
-    # print(s.objcode_3)
-    # print(s.objcode_3_imm)
-    # print(ass.var)
-    # print(s.objectCodes)
-    # print(s.inst_ref)
-    print("hhhhhh")
-    # print(s.hRecord)
+    
     print(s.inst_ref)
     ass.get_assembly_code(s, f)
-    # print(ass.wb)
-    ass.get_resw(s)
     print(ass.locationCounter)
-    print(ass.reswb)
-    # print (int("1000", 16) < int("101F", 16) < int("101E", 16))
 
 if __name__ == '__main__':
    
